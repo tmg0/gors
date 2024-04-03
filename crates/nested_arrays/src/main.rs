aad6e16d460d88ee6a865d481e60d281316e5003
tmg0 <zekun.jin@foxmail.com>
2024-04-03 15:44:44 +0800

    feat: nested arrays

---
+++ crates/nested_arrays/src/main.rs
@@ -0,0 +1,31 @@
+fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
+    let mut result = [[0; 3]; 3];
+    for i in 0..3 {
+        for j in 0..3 {
+            result[j][i] = matrix[i][j]
+        }
+    }
+    result
+}
+
+#[test]
+fn test_transpose() {
+    let matrix = [[101, 102, 103], [201, 202, 203], [301, 302, 303]];
+    let transposed = transpose(matrix);
+    assert_eq!(
+        transposed,
+        [[101, 201, 301], [102, 202, 302], [103, 203, 303],]
+    );
+}
+
+fn main() {
+    let matrix = [
+        [101, 102, 103], // <-- the comment makes rustfmt add a newline
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+
+    println!("matrix: {:#?}", matrix);
+    let transposed = transpose(matrix);
+    println!("transposed: {:#?}", transposed);
+}
